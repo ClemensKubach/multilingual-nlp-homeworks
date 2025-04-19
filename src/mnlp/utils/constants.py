849c82ad675f20fdd9881567de71ece49326bb07
Clemens Kubach <clemens.kubach@gmail.com>
2025-04-19 17:56:23 +0200

    refactor

--- src/mnlp/utils/constants.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-_utils_dir = Path(__file__).parent
-_src_dir = _utils_dir.parent.parent
-REPO_ROOT = _src_dir.parent
-DATA_DIR = REPO_ROOT / "data"
-LOG_DIR = REPO_ROOT / "logs"
